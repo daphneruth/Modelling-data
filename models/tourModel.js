3ff6f7f3f8b0aa4fa9dce0d776fb15f1a73c7b1e
Daphne <daphneruth304@gmail.com>
2022-10-18 19:38:19 +0300

    updated

--- models/tourModel.js
+++ models/tourModel.js
@@ -120,7 +120,7 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
-// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+//  runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
