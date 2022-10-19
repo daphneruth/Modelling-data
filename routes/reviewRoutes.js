1a5e9636936ec8e770e290916c83e8e7f10a946f
Daphne <daphneruth304@gmail.com>
2022-10-19 07:07:30 +0300

    updated

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const reviewController = require('./../controllers/reviewController');
-const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(reviewController.setTourUserIds, reviewController.createReview);
 
 module.exports = router;
