2f579acd59d5686bb603b916d4c5043df45b1687
Daphne <daphneruth304@gmail.com>
2022-10-19 07:09:09 +0300

    protecting route

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
-  .post(reviewController.setTourUserIds, reviewController.createReview);
+  .post(
+    authController.restrictTo('user'),
+    reviewController.setTourUserIds,
+    reviewController.createReview
+  );
 
 module.exports = router;
