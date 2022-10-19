048e6119164dccf2f7f4f786473ef224eed8b0a0
Daphne <daphneruth304@gmail.com>
2022-10-19 06:57:28 +0300

    updated

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,6 +2,7 @@ const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
   res.json({
     status: 'success',
     results: this.getAllReviews.length,
