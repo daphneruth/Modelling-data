a708e3924d186c6e21625717f23f802e8b8bf696
Daphne <daphneruth304@gmail.com>
2022-10-20 08:06:43 +0300

    updated

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
-const APIFeatures = require('./../utils/apiFeatures');
+//const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
 const AppError = require('./../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
