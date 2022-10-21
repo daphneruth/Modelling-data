30f805d812c7cc8eb60a4e6ca50437ae4cedc49c
Daphne <daphneruth304@gmail.com>
2022-10-21 08:21:36 +0300

    updated

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -25,3 +25,14 @@ exports.createOne = Model =>
       }
     });
   });
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
